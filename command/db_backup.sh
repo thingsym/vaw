623cb0c9536124879265231402faed5593bfaa38
thingsym <thingsym@gmail.com>
2015-05-28 22:19:14 +0900

    add command db_backup.sh

---
+++ command/db_backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ -d /vagrant/backup ]]; then
+	echo "exist /vagrant/backup"
+else
+	mkdir /vagrant/backup
+	echo "create /vagrant/backup"
+fi
+
+wp db export /vagrant/backup/backup-`date +%Y%m%d%H%M%S`.sql
+
+exit
