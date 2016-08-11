f17a2167ab44a237c962a6d5fe19f358d5bbea6d
thingsym <thingsym@gmail.com>
2016-08-11 23:54:58 +0900

    add nodenv

---
+++ spec/localhost/nodenv_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+require 'shellwords'
+
+if property["develop_tools"] then
+
+  ['0.11.2'].each do |node_version|
+    describe command("nodenv versions | grep #{node_version}") do
+      let(:disable_sudo) { true }
+      its(:stdout) { should match(/#{Regexp.escape(node_version)}/) }
+    end
+  end
+
+  describe command('node -v') do
+    let(:disable_sudo) { true }
+    its(:stdout) { should match '0.11.2' }
+  end
+
+  describe command('nodenv global') do
+    let(:disable_sudo) { true }
+    its(:stdout) { should match '0.11.2' }
+  end
+
+  describe file('/home/vagrant/.bash_profile') do
+    its(:content) { should match /export PATH=\$HOME\/\.nodenv\/bin:\$PATH/ }
+    its(:content) { should match /eval "\$\(nodenv init \-\)"/ }
+  end
+
+  describe file('/home/vagrant/.bashrc') do
+    its(:content) { should match /export PATH=\$HOME\/\.nodenv\/bin:\$PATH/ }
+    its(:content) { should match /eval "\$\(nodenv init \-\)"/ }
+  end
+
+  describe file('/home/vagrant/.nodenv/plugins/node-build') do
+    it { should be_directory }
+  end
+
+  describe file('/home/vagrant/.nodenv/plugins/nodenv-default-packages') do
+    it { should be_directory }
+  end
+
+  describe command('npm -v') do
+    let(:disable_sudo) { true }
+    its(:exit_status) { should eq 0 }
+  end
+end
