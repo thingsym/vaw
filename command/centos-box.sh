5fcdac7c4afce2516b05ff4c9c773688cfab4a12
thingsym <thingsym@gmail.com>
2017-06-15 23:37:22 +0900

    fix centos-box.sh

--- command/centos-box.sh
+++ command/centos-box.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo rm /etc/sysconfig/network-scripts/ifcfg-eth1
+sudo rm -f /etc/sysconfig/network-scripts/ifcfg-eth1
 sudo ln -s -f /dev/null /etc/udev/rules.d/70-persistent-net.rules
 sudo rm -rf /dev/.udev/
 sudo rm -f /lib/udev/rules.d/75-persistent-net-generator.rules
