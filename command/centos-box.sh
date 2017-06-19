4afedc0790c126e0d2f2c0653a2049df9c843042
thingsym <thingsym@gmail.com>
2017-06-19 20:25:17 +0900

    dalete 70-persistent-ipoib.rules

--- command/centos-box.sh
+++ command/centos-box.sh
@@ -5,8 +5,13 @@ set -e
 sudo rm -f /etc/sysconfig/network-scripts/ifcfg-eth1
 sudo ln -s -f /dev/null /etc/udev/rules.d/70-persistent-net.rules
 sudo rm -rf /dev/.udev/
+sudo rm -f /etc/udev/rules.d/70-persistent-ipoib.rules
 sudo rm -f /lib/udev/rules.d/75-persistent-net-generator.rules
 sudo du -sh /var/cache/yum
 sudo yum clean all
+
+# swapoff /swapfile
+# dd if=/dev/zero of=/swapfile bs=1M
+# rm -f /swapfile
 
 exit
