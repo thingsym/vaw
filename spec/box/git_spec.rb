850d617eab6a368707ea5160693ae6729702850a
thingsym <thingsym@gmail.com>
2019-06-05 21:01:48 +0900

    perf: bump up git version 2 or later

--- spec/box/git_spec.rb
+++ spec/box/git_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 require 'shellwords'
 
-describe package('git') do
+describe package('git2u'), :if => os[:family] == 'redhat' do
   it { should be_installed }
+end
+
+describe package('git'), :if => os[:family] == 'debian' || os[:family] == 'ubuntu' do
+  it { should be_installed }
+end
+
+describe command('which git') do
+  let(:sudo_options) { '-u vagrant -i' }
+  its(:exit_status) { should eq 0 }
 end
