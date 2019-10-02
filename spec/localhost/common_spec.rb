b2b80249e4e3f718892a150f9a07bbbfe1b51900
thingsym <thingsym@gmail.com>
2019-10-02 19:32:20 +0900

    perf: improve kernel parameter

--- spec/localhost/common_spec.rb
+++ spec/localhost/common_spec.rb
@@ -77,10 +77,65 @@ describe 'Linux kernel parameters' do
   context linux_kernel_parameter('net.core.netdev_max_backlog') do
     its(:value) { should be 4096 }
   end
+
+  context linux_kernel_parameter('net.ipv4.ip_local_port_range') do
+    its(:value) { should match /1024\t65535/ }
+  end
   context linux_kernel_parameter('net.ipv4.tcp_tw_reuse') do
     its(:value) { should eq 1 }
   end
   context linux_kernel_parameter('net.ipv4.tcp_fin_timeout') do
     its(:value) { should eq 10 }
   end
+  context linux_kernel_parameter('net.ipv4.tcp_keepalive_time') do
+    its(:value) { should eq 10 }
+  end
+  context linux_kernel_parameter('net.ipv4.tcp_keepalive_probes') do
+    its(:value) { should eq 2 }
+  end
+  context linux_kernel_parameter('net.ipv4.tcp_keepalive_intvl') do
+    its(:value) { should eq 3 }
+  end
+
+  context linux_kernel_parameter('net.ipv4.tcp_window_scaling') do
+    its(:value) { should eq 1 }
+  end
+  context linux_kernel_parameter('net.core.rmem_max') do
+    its(:value) { should eq 16777216 }
+  end
+  context linux_kernel_parameter('net.core.wmem_max') do
+    its(:value) { should eq 16777216 }
+  end
+  context linux_kernel_parameter('net.ipv4.tcp_rmem') do
+    its(:value) { should match /4096\t349520\t16777216/ }
+  end
+  context linux_kernel_parameter('net.ipv4.tcp_wmem') do
+    its(:value) { should match /4096\t65536\t16777216/ }
+  end
+
+  context linux_kernel_parameter('net.ipv4.tcp_no_metrics_save') do
+    its(:value) { should eq 1 }
+  end
+  context linux_kernel_parameter('net.ipv4.conf.all.rp_filter') do
+    its(:value) { should eq 1 }
+  end
+  context linux_kernel_parameter('net.ipv4.tcp_rfc1337') do
+    its(:value) { should eq 1 }
+  end
+  context linux_kernel_parameter('net.ipv4.tcp_syncookies') do
+    its(:value) { should eq 1 }
+  end
+  context linux_kernel_parameter('vm.overcommit_memory') do
+    its(:value) { should eq 2 }
+  end
+  context linux_kernel_parameter('vm.overcommit_ratio') do
+    its(:value) { should eq 99 }
+  end
+  context linux_kernel_parameter('kernel.panic') do
+    its(:value) { should eq 30 }
+  end
+  context linux_kernel_parameter('vm.panic_on_oom') do
+    its(:value) { should eq 1 }
+  end
+
 end
