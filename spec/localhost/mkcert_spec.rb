a41a339af1dcea00939eab53bcaa9a733872351e
thingsym <thingsym@gmail.com>
2021-06-15 17:51:13 +0900

    test: fix test case

--- spec/localhost/mkcert_spec.rb
+++ spec/localhost/mkcert_spec.rb
@@ -4,14 +4,18 @@ require 'shellwords'
 # require "pp"
 # pp property
 
+describe file('/etc/mkcert') do
+  it { should be_directory }
+end
+
 if property["ssl"] then
-  describe file("/vagrant/mkcert/cert.pem") do
+  describe file("/etc/mkcert/cert.pem") do
     it { should be_file }
     it { should be_owned_by 'vagrant' }
     it { should be_grouped_into 'vagrant' }
   end
 
-  describe file("/vagrant/mkcert/privkey.pem") do
+  describe file("/etc/mkcert/privkey.pem") do
     it { should be_file }
     it { should be_owned_by 'vagrant' }
     it { should be_grouped_into 'vagrant' }
