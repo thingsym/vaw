1bdde3b59c920d84b6cf3e333b431ae833b4e216
thingsym <thingsym@gmail.com>
2021-07-22 21:52:08 +0900

    test: fix test case

--- spec/box/phpenv_spec.rb
+++ spec/box/phpenv_spec.rb
@@ -158,16 +158,12 @@ if property["php_version"] != 0 then
       its(:value) { should eq '32M' }
     end
 
-    context  php_config('default_charset') do
+    context php_config('default_charset') do
       its(:value) { should eq 'UTF-8' }
     end
 
-    context  php_config('mbstring.language') do
+    context php_config('mbstring.language') do
       its(:value) { should eq 'neutral' }
-    end
-
-    context  php_config('mbstring.internal_encoding') do
-      its(:value) { should eq 'UTF-8' }
     end
 
     context php_config('date.timezone') do
